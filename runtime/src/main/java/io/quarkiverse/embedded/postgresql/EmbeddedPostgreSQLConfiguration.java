1912ab5c6ddbb98a7829dfc41fbe375fc2b27bf7
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2023-02-20 11:12:36 +0100

    Cristiano's comments

--- runtime/src/main/java/io/quarkiverse/embedded/postgresql/EmbeddedPostgreSQLConfiguration.java
+++ runtime/src/main/java/io/quarkiverse/embedded/postgresql/EmbeddedPostgreSQLConfiguration.java
@@ -6,7 +6,7 @@ import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 
-@ConfigRoot(phase = ConfigPhase.RUN_TIME, prefix = "quarkus.embedded.postgresql")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME, name = "embedded.postgresql")
 public class EmbeddedPostgreSQLConfiguration {
 
     /**
