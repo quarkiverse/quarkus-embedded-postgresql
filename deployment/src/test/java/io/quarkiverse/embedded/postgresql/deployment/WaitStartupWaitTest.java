c08cd33e6b9f65dad37dee9bcd0fa826c0d5cb15
Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>
2023-06-12 21:50:06 +0200

    Fixing formatting (#39)

--- deployment/src/test/java/io/quarkiverse/embedded/postgresql/deployment/WaitStartupWaitTest.java
+++ deployment/src/test/java/io/quarkiverse/embedded/postgresql/deployment/WaitStartupWaitTest.java
@@ -2,12 +2,13 @@ package io.quarkiverse.embedded.postgresql.deployment;
 
 import java.io.IOException;
 
-import io.quarkus.test.QuarkusUnitTest;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
 
 public class WaitStartupWaitTest {
 
