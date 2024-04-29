776c1c64996d5c09b34e366a57914532be0a0041
Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>
2024-04-29 13:58:10 +0200

    Removing code duplication (#87)
    More refactor

--- runtime/src/main/java/io/quarkiverse/embedded/postgresql/EmbeddedPostgreSQLDBUtils.java
+++ runtime/src/main/java/io/quarkiverse/embedded/postgresql/EmbeddedPostgreSQLDBUtils.java
@@ -1,20 +1,33 @@
 package io.quarkiverse.embedded.postgresql;
 
-import static io.quarkiverse.embedded.postgresql.EmbeddedPostgreSQLConfigUtils.*;
+import static io.quarkiverse.embedded.postgresql.EmbeddedPostgreSQLConfigUtils.DEFAULT_DATABASE;
+import static io.quarkiverse.embedded.postgresql.EmbeddedPostgreSQLConfigUtils.DEFAULT_PASSWORD;
+import static io.quarkiverse.embedded.postgresql.EmbeddedPostgreSQLConfigUtils.DEFAULT_USERNAME;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.Duration;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.sql.DataSource;
 
+import org.jboss.logging.Logger;
+
 import io.zonky.test.db.postgres.embedded.EmbeddedPostgres;
+import io.zonky.test.db.postgres.embedded.EmbeddedPostgres.Builder;
 
 public class EmbeddedPostgreSQLDBUtils {
 
-    public static void createDatabases(EmbeddedPostgres pg, Collection<String> dbNames,
+    private static final Logger logger = Logger.getLogger(EmbeddedPostgreSQLDBUtils.class);
+
+    private static void createDatabases(EmbeddedPostgres pg, Collection<String> dbNames,
             String userName) {
         pg.getDatabase(DEFAULT_USERNAME, DEFAULT_DATABASE);
         dbNames.forEach(ds -> createDatabase(pg.getPostgresDatabase(), ds, userName));
@@ -35,4 +48,46 @@ public class EmbeddedPostgreSQLDBUtils {
         }
     }
 
+    public static EmbeddedPostgres startPostgres(Optional<Integer> port, Map<String, String> dbNames, String stringType,
+            Optional<Long> startUpWait, Optional<String> dataDir) {
+        Builder builder = EmbeddedPostgres.builder();
+        builder.setConnectConfig("stringtype", stringType);
+        port.ifPresent(p -> {
+            logger.infov("PG port set to {0}", p);
+            builder.setPort(p);
+        });
+        startUpWait.ifPresent(
+                timeout -> {
+                    logger.infov("PG startup timeout set to {0}", timeout);
+                    builder.setPGStartupWait(Duration.ofMillis(timeout));
+                });
+        dataDir.ifPresent(path -> {
+            logger.infov("Setting embedded postgresql data dir to {0}", path);
+            builder.setDataDirectory(path);
+            builder.setCleanDataDirectory(false);
+        });
+        try {
+            EmbeddedPostgres pg = builder.start();
+            logger.infov(
+                    "Embedded Postgres started at port \"{0,number,#}\" with database \"{1}\", user \"{2}\" and password \"{3}\"",
+                    pg.getPort(), DEFAULT_DATABASE, DEFAULT_USERNAME, DEFAULT_PASSWORD);
+            createDatabases(pg, dbNames.values(), DEFAULT_USERNAME);
+            return pg;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+    }
+
+    public static void close(Closeable pg) {
+        try {
+            logger.info("Embedded Postgres shutting down...");
+            pg.close();
+        } catch (IOException e) {
+            logger.warn("Error closing Embedded Postgres", e);
+        }
+    }
+
+    private EmbeddedPostgreSQLDBUtils() {
+    }
 }
