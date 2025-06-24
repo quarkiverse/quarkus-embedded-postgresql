24200447d76f3c2bd2cc2357b16d414326e804d5
Dmitrii Tikhomirov <treblereel@users.noreply.github.com>
2025-06-24 14:39:47 -0700

    ISSUE_143: use @ConfigProperty for quarkus.datasource.jdbc.url in Dev Mode (#147)

--- runtime/src/main/java/io/quarkiverse/embedded/postgresql/devui/EmbeddedPostgreSQLJsonRpcService.java
+++ runtime/src/main/java/io/quarkiverse/embedded/postgresql/devui/EmbeddedPostgreSQLJsonRpcService.java
@@ -6,22 +6,34 @@ import java.util.regex.Pattern;
 
 import jakarta.inject.Inject;
 
-import io.quarkus.devui.runtime.config.ConfigDescription;
 import io.quarkus.devui.runtime.config.ConfigDescriptionBean;
+import io.quarkus.runtime.LaunchMode;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import static io.quarkus.runtime.LaunchMode.DEVELOPMENT;
 
 public class EmbeddedPostgreSQLJsonRpcService {
 
     @Inject
     ConfigDescriptionBean configDescriptionBean;
 
+    @ConfigProperty(name = "quarkus.datasource.jdbc.url")
+    Optional<String> jdbcUrl;
+
     public int getDatasourcePort() {
-        Optional<ConfigDescription> config = configDescriptionBean.getAllConfig().stream()
-                .filter(c -> c.getName().equalsIgnoreCase("quarkus.datasource.jdbc.url")).findFirst();
+        String port = LaunchMode.current().equals(DEVELOPMENT) && jdbcUrl.isPresent() ? jdbcUrl.get()
+                : configDescriptionBean.getAllConfig().stream()
+                        .filter(c -> c.getName().equalsIgnoreCase("quarkus.datasource.jdbc.url"))
+                        .map(c -> c.getConfigValue().getValue())
+                        .findFirst()
+                        .orElseThrow(() -> new IllegalStateException(
+                                "No JDBC URL found in configuration. Please ensure 'quarkus.datasource.jdbc.url' is set."));
+
         // Define a regex pattern to match numbers
         Pattern pattern = Pattern.compile("\\d+");
 
         // Create a matcher with the input string
-        Matcher matcher = pattern.matcher(config.get().getConfigValue().getValue());
+        Matcher matcher = pattern.matcher(port);
 
         // Find and print all numbers in the input string
         while (matcher.find()) {
